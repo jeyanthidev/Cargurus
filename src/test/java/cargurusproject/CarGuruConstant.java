e1db16af1b0b5d37f710f0edd6d0ed8ed3032524
jeyanthidevush <jeyanthidev@gmail.com>
2019-08-15 22:13:08 -0500

    Added cargurusproject/CarGuruConstant

---
+++ src/test/java/cargurusproject/CarGuruConstant.java
@@ -0,0 +1,7 @@
+package cargurusproject;
+
+public interface CarGuruConstant {
+
+	String WEBDRIVER="webdriver.chrome.driver";
+	String WEBDRIVER_PATH="C:\\data\\chromedriver_win32\\chromedriver.exe";
+}
