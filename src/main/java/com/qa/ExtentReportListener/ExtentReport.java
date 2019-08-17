8bcad6fa1f527187be544d11d95c3ad7cbf01f72
jeyanthidevush <jeyanthidev@gmail.com>
2019-08-17 09:51:21 -0500

    CA-3 update extent report to capture image

--- src/main/java/com/qa/ExtentReportListener/ExtentReport.java
+++ src/main/java/com/qa/ExtentReportListener/ExtentReport.java
@@ -1,79 +1,86 @@
 package com.qa.ExtentReportListener;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
-	
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.IReporter;
+import org.testng.IResultMap;
+import org.testng.ISuite;
+import org.testng.ISuiteResult;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.xml.XmlSuite;
 
-	import java.io.File;
-	import java.util.Calendar;
-	import java.util.Date;
-	import java.util.List;
-	import java.util.Map;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
-	import org.testng.IReporter;
-	import org.testng.IResultMap;
-	import org.testng.ISuite;
-	import org.testng.ISuiteResult;
-	import org.testng.ITestContext;
-	import org.testng.ITestResult;
-	import org.testng.xml.XmlSuite;
+public class ExtentReport implements IReporter {
+	private ExtentReports extent;
 
-	import com.relevantcodes.extentreports.ExtentReports;
-	import com.relevantcodes.extentreports.ExtentTest;
-	import com.relevantcodes.extentreports.LogStatus;
+	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+		System.out.println("Inside ExtentReport::generateReport");
+		extent = new ExtentReports(outputDirectory + File.separator + "Extent.html", true);
 
-	public class ExtentReport implements IReporter {
-		private ExtentReports extent;
+		for (ISuite suite : suites) {
+			Map<String, ISuiteResult> result = suite.getResults();
 
-		public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
-				String outputDirectory) {
-			extent = new ExtentReports(outputDirectory + File.separator
-					+ "Extent.html", true);
+			for (ISuiteResult r : result.values()) {
+				ITestContext context = r.getTestContext();
 
-			for (ISuite suite : suites) {
-				Map<String, ISuiteResult> result = suite.getResults();
-
-				for (ISuiteResult r : result.values()) {
-					ITestContext context = r.getTestContext();
-
-					buildTestNodes(context.getPassedTests(), LogStatus.PASS);
-					buildTestNodes(context.getFailedTests(), LogStatus.FAIL);
-					buildTestNodes(context.getSkippedTests(), LogStatus.SKIP);
-				}
-			}
-
-			extent.flush();
-			extent.close();
-		}
-
-		private void buildTestNodes(IResultMap tests, LogStatus status) {
-			ExtentTest test;
-
-			if (tests.size() > 0) {
-				for (ITestResult result : tests.getAllResults()) {
-					test = extent.startTest(result.getMethod().getMethodName());
-
-					test.setStartedTime(getTime(result.getStartMillis()));
-					test.setEndedTime(getTime(result.getEndMillis()));
-
-					for (String group : result.getMethod().getGroups())
-						test.assignCategory(group);
-
-					if (result.getThrowable() != null) {
-						test.log(status, result.getThrowable());
-					} else {
-						test.log(status, "Test " + status.toString().toLowerCase()
-								+ "ed");
-					}
-
-					extent.endTest(test);
-				}
+				buildTestNodes(context.getPassedTests(), LogStatus.PASS);
+				buildTestNodes(context.getFailedTests(), LogStatus.FAIL);
+				buildTestNodes(context.getSkippedTests(), LogStatus.SKIP);
 			}
 		}
 
-		private Date getTime(long millis) {
-			Calendar calendar = Calendar.getInstance();
-			calendar.setTimeInMillis(millis);
-			return calendar.getTime();
+		extent.flush();
+		extent.close();
+	}
+
+	private void buildTestNodes(IResultMap tests, LogStatus status) {
+		ExtentTest test;
+		String screenshotPath;
+		if (tests.size() > 0) {
+			for (ITestResult result : tests.getAllResults()) {
+				test = extent.startTest(result.getMethod().getMethodName());
+
+				test.setStartedTime(getTime(result.getStartMillis()));
+				test.setEndedTime(getTime(result.getEndMillis()));
+
+				for (String group : result.getMethod().getGroups())
+					test.assignCategory(group);
+
+				if (result.getThrowable() != null) {
+					test.log(status, result.getThrowable());
+				} else {
+					test.log(status, "Test " + status.toString().toLowerCase() + "ed");
+				}
+				if(result.getStatus()==ITestResult.FAILURE){
+					screenshotPath =(String) result.getAttribute("screenShotPath");
+					System.out.println("screenShotPath::"+screenshotPath);
+					test.log(LogStatus.FAIL, test.addScreenCapture(screenshotPath)); 
+				}
+				
+				extent.endTest(test);
+			}
 		}
 	}
 
+	private Date getTime(long millis) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(millis);
+		return calendar.getTime();
+	}
+	
+	
+}
